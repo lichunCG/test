9aed676b45073d1036edba4357d9e6391f85ea23
WeiTing.Lian <lianweiting1995@gmail.com>
2018-06-07 16:04:53 +0800

    “modify”

--- src/main/java/com/example/test/TestApplication.java
+++ src/main/java/com/example/test/TestApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestApplication {
-//测试1234
+//测试12345
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args);
     }
